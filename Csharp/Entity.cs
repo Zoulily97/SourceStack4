4936af66dc1104a308e025b9c05ed82e62d9751a
freeflying1222 <2282636022@qq.com>
2020-11-08 16:27:37 +0800

    good

--- Csharp/Entity.cs
+++ Csharp/Entity.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Csharp
 {
     //4、再为之前所有类（含User、HelpMoney等）抽象一个基类：Entity，
-    //包含一个只读的Id属性。试一试，Suggest能有Id属性么？
+    //包含一个只读的Id属性。试一试，Suggest能有Id属性么？good
    public  class Entity
     {
         public  int Id { get; }
