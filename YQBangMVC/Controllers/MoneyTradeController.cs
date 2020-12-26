aa4e8c031818f4fec53cdf32a2f59d6c72b323b9
zouzou <1151935939@qq.com>
2020-12-26 21:41:12 +0800

    在MVC项目上完成求督促|帮帮币出售页面

---
+++ YQBangMVC/Controllers/MoneyTradeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YQBangMVC.Controllers
+{
+    public class MoneyTradeController : Controller
+    {
+        // GET: MoneyTrade
+        public ActionResult Sale()
+        {
+            return View();
+        }
+    }
+}
