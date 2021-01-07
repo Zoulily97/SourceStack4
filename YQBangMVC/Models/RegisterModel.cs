0fa3cb90c0b219becbff07fa1d15413ec7f6c8fa
zouzou <1151935939@qq.com>
2021-01-07 11:06:25 +0800

    js 声明变量

---
+++ YQBangMVC/Models/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace YQBangMVC.Models
+{
+    public class RegisterModel
+    {
+
+        [Display(Name = "用户名")]
+        [Required(ErrorMessage = "用户名不能为空")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "性别不能为空")]
+        public bool IsMale { get; set; }
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        public string InviterNumber { set; get; }
+        public string InviterName { get; set; }
+    }
+}
